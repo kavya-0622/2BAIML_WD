5663423eecc1269f494bbc30836e4e2dcf5c997c
Kavya Sharma <kavya22666@gmail.com>
2026-09-16 19:11:09 +0530

    temp

---
+++ temp.js
@@ -0,0 +1,58 @@
+// let FirstName = "Vikas";
+// let LastName = "Choudhary";
+
+// console.log("My FirstName is : " + FirstName)
+// console.log("My LastName is : " + LastName)
+
+// console.log(`my FirstName is ${FirstName}`);
+// console.log(`my LastName is ${LastName}`);
+// console.log(`My name is : ${FirstName} ${LastName}`);
+
+
+
+// // USING FUNCTION :--
+// let FirstName = "Kavya";
+// let LastName = "Sharma";
+
+// function FullName(First,Last){
+//     return `${First} ${Last}`
+// }
+
+// let Name = `Hello ${FullName(FirstName, LastName)}`;
+// console.log(Name);
+
+
+//ARROW FUNCTION :--
+
+Hello = () => console.log("Hello Kavya");
+Hello();
+let marks = 96;
+Physics = (marks) => {
+    return marks;
+};
+console.log(Physics(marks));
+
+
+
+
+// function hello() {
+//     console.log("Hello, World!");
+// }  
+// let Hello = function() {
+//     console.log("Hello, World!");
+// }
+// Hello();
+
+// console.log("now by arror function !!");
+// let Hello1 = () => {
+//     console.log("Hello, World!");
+// }
+// Hello1();
+
+// let hello2 = (name) => {
+//     console.log(`Hello, ${name}!`);
+// }
+// hello2("World");
+
+// let hello3 = (firstname, lastname) => `Hello, ${firstname} ${lastname}!`;
+// console.log(hello3("vikas", "chaudhary"));
